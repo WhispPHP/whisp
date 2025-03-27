25690ad08c325eac71017ad7c569fd4580ee1792
Ashley Hindle <hey@ashleyhindle.com>
2025-03-27 19:14:53 +0000

    feat: add sleep to howdy app for Ctrl+C testing

--- examples/howdy.php
+++ examples/howdy.php
@@ -4,7 +4,9 @@ use function Laravel\Prompts\text;
 
 require __DIR__.'/vendor/autoload.php';
 
-$name = text('What is your name?', 'John');
+$name = text('What is your name?', required: true, placeholder: 'John Doe');
 
 echo "\n\033[48;5;25m\033[1;97mHowdy {$name},\033[0m\n";
 echo "\nNice to meet you, you must be a good egg!\n\n";
+
+sleep(2);
