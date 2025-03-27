1365507486caf6cb587fc5f740e80f797e36026d
Ashley Hindle <hey@ashleyhindle.com>
2025-03-27 19:22:53 +0000

    refactor: update Ffi constants based on C app that extracted them

--- src/Ffi.php
+++ src/Ffi.php
@@ -9,7 +9,7 @@ class Ffi
 {
     private \FFI $ffi;
 
-    private array $constants;
+    public array $constants;
 
     public function __construct()
     {
@@ -22,6 +22,9 @@ class Ffi
         // Define different FFI based on OS
         if (PHP_OS === 'Darwin') {
             $this->ffi = \FFI::cdef('
+                #define _DARWIN_C_SOURCE
+
+                // Type definitions
                 typedef unsigned int mode_t;
                 typedef unsigned long tcflag_t;
                 typedef unsigned char cc_t;
@@ -33,6 +36,9 @@ class Ffi
                 int *__error(void);
                 int tcgetattr(int fd, struct termios *termios_p);
                 int tcsetattr(int fd, int optional_actions, const struct termios *termios_p);
+                int open(const char *pathname, int flags, ...);
+                int close(int fd);
+                int fcntl(int fd, int cmd, ...);
 
                 // Type definitions
                 typedef char ptsname_t[128];
@@ -43,7 +49,7 @@ class Ffi
                     tcflag_t c_oflag;
                     tcflag_t c_cflag;
                     tcflag_t c_lflag;
-                    cc_t     c_cc[20];
+                    cc_t     c_cc[20];  // NCCS = 20 on macOS
                     speed_t  c_ispeed;
                     speed_t  c_ospeed;
                 };
@@ -56,23 +62,64 @@ class Ffi
                 };
             ', $libcName);
 
+            // These constants are obtained from running the get_termios_constants program on macOS
             $this->constants = [
+                // ioctl requests
                 'TIOCPTYGNAME' => 0x40807453,
                 'TIOCPTYGRANT' => 0x20007454,
                 'TIOCPTYUNLK' => 0x20007452,
+                'TIOCSWINSZ' => 0x80087467,
+                'TIOCSCTTY' => 0x20007461,  // Set controlling terminal
+                'TIOCSPGRP' => 0x80047476,  // Set process group
+
+                // Terminal settings
                 'TCSANOW' => 0,
-                'ICANON' => 0x00000100,
+
+                // Local mode flags (c_lflag)
+                'ICANON' => 0x00000002,
                 'ECHO' => 0x00000008,
                 'ECHOE' => 0x00000002,
                 'ECHOK' => 0x00000004,
                 'ECHONL' => 0x00000010,
                 'ISIG' => 0x00000080,
                 'IEXTEN' => 0x00000400,
+
+                // Input mode flags (c_iflag)
                 'ICRNL' => 0x00000100,  // Convert CR to NL on input
-                'TIOCSWINSZ' => 0x80087467,
+
+                // File flags
+                'O_RDWR' => 0x0002,
+                'O_NOCTTY' => 0x20000,
+                'O_NONBLOCK' => 0x0004,   // Non-blocking I/O
+                'FIOCLEX' => 0x20006601,  // Set close-on-exec flag
+                'FIONCLEX' => 0x20006602, // Clear close-on-exec flag
+
+                // fcntl commands
+                'F_GETFL' => 3,
+                'F_SETFL' => 4,
+                'F_SETFD' => 2,
+                'F_GETFD' => 1,
+                'FD_CLOEXEC' => 1,        // Close-on-exec flag
+
+                // cc_t array indices
+                'VINTR' => 8,
+                'VQUIT' => 9,
+                'VERASE' => 3,
+                'VKILL' => 5,
+                'VEOF' => 0,
+                'VEOL' => 1,
+                'VEOL2' => 2,
+                'VSTART' => 12,
+                'VSTOP' => 13,
+                'VSUSP' => 10,
+                'VREPRINT' => 6,
+                'VWERASE' => 4,
+                'VLNEXT' => 14,
+                'VDSUSP' => 11,
+                'VSTATUS' => 18,
             ];
         } else {
-            // Linux version - with correct struct layout and constants from test output
+            // Linux version
             $this->ffi = \FFI::cdef('
                 typedef unsigned int mode_t;
                 typedef unsigned int tcflag_t;  // 4 bytes on Linux
@@ -80,17 +127,19 @@ class Ffi
                 typedef unsigned int speed_t;   // 4 bytes on Linux
 
                 // Function declarations
-                int ioctl(int fd, unsigned long request, void* arg);
+                int ioctl(int fd, unsigned long request, ...);
                 char* strerror(int errnum);
                 extern int errno;               // Global errno
                 int tcgetattr(int fd, struct termios* termios_p);
                 int tcsetattr(int fd, int optional_actions, const struct termios* termios_p);
+                int open(const char *pathname, int flags, ...);
+                int close(int fd);
+                int fcntl(int fd, int cmd, ...);
 
                 // Type definitions
                 typedef char ptsname_t[128];
 
                 // Struct definitions
-                #pragma pack(1)  // Important: ensure correct struct packing
                 struct termios {
                     tcflag_t c_iflag;          // offset 0
                     tcflag_t c_oflag;          // offset 4
@@ -108,22 +157,89 @@ class Ffi
                     unsigned short ws_xpixel;   // offset 4
                     unsigned short ws_ypixel;   // offset 6
                 };
-                #pragma pack()  // Reset packing
             ', $libcName);
 
             $this->constants = [
+                // ioctl requests
                 'TIOCGPTN' => 0x80045430,    // Get PTY number
                 'TIOCSPTLCK' => 0x40045431,  // Lock/unlock PTY
-                'TCSANOW' => 0,
-                'ICANON' => 0x2,             // Updated Linux values
-                'ECHO' => 0x8,
-                'ECHOE' => 0x10,
-                'ECHOK' => 0x20,
-                'ECHONL' => 0x40,
-                'ISIG' => 0x1,
-                'IEXTEN' => 0x8000,
-                'ICRNL' => 0x100,
                 'TIOCSWINSZ' => 0x5414,      // Correct Linux value
+                'TIOCSCTTY' => 0x540E,       // Set controlling terminal
+                'TIOCSPGRP' => 0x5410,       // Set process group
+
+                // Terminal settings
+                'TCSANOW' => 0,
+
+                // Local mode flags (c_lflag)
+                'ISIG' => 0x00000001,
+                'ICANON' => 0x00000002,
+                'ECHO' => 0x00000008,
+                'ECHOE' => 0x00000010,
+                'ECHOK' => 0x00000020,
+                'ECHONL' => 0x00000040,
+                'NOFLSH' => 0x00000080,
+                'TOSTOP' => 0x00000100,
+                'IEXTEN' => 0x00008000,
+                'ECHOCTL' => 0x00000200,
+                'ECHOKE' => 0x00000800,
+                'PENDIN' => 0x00004000,
+                'XCASE' => 0x00000004,
+
+                // Input mode flags (c_iflag)
+                'IGNPAR' => 0x00000004,
+                'PARMRK' => 0x00000008,
+                'INPCK' => 0x00000010,
+                'ISTRIP' => 0x00000020,
+                'INLCR' => 0x00000040,
+                'IGNCR' => 0x00000080,
+                'ICRNL' => 0x00000100,
+                'IUCLC' => 0x00000200,
+                'IXON' => 0x00000400,
+                'IXANY' => 0x00000800,
+                'IXOFF' => 0x00001000,
+                'IMAXBEL' => 0x00002000,
+                'IUTF8' => 0x00004000,
+
+                // Output flags (c_oflag)
+                'OPOST' => 0x00000001,
+                'OLCUC' => 0x00000002,
+                'ONLCR' => 0x00000004,
+                'OCRNL' => 0x00000008,
+                'ONOCR' => 0x00000010,
+                'ONLRET' => 0x00000020,
+
+                // Control flags (c_cflag)
+                'CS7' => 0x00000020,
+                'CS8' => 0x00000030,
+                'PARENB' => 0x00000100,
+                'PARODD' => 0x00000200,
+
+                // fcntl commands
+                'F_GETFL' => 3,
+                'F_SETFL' => 4,
+                'F_SETFD' => 2,
+                'F_GETFD' => 1,
+                'FD_CLOEXEC' => 1,
+
+                // File flags
+                'O_RDWR' => 0x0002,
+                'O_NOCTTY' => 0x100,
+                'O_NONBLOCK' => 0x800,
+
+                // cc_t array indices
+                'VINTR' => 0,
+                'VQUIT' => 1,
+                'VERASE' => 2,
+                'VKILL' => 3,
+                'VEOF' => 4,
+                'VEOL' => 11,
+                'VEOL2' => 16,
+                'VSTART' => 8,
+                'VSTOP' => 9,
+                'VSUSP' => 10,
+                'VREPRINT' => 12,
+                'VWERASE' => 14,
+                // VDSUSP and VSTATUS are not defined on Linux
             ];
         }
     }
@@ -143,36 +259,46 @@ class Ffi
     public function getSlaveNameFromMaster(int $masterFd): string
     {
         if (PHP_OS === 'Darwin') {
+            error_log('FFI getSlaveNameFromMaster: Using macOS method');
             // macOS: Use TIOCPTYGNAME to get the slave name
             $namebuf = $this->ffi->new('ptsname_t');
             \FFI::memset($namebuf, 0, 128);
 
-            $ret = $this->ffi->ioctl($masterFd, $this->constants['TIOCPTYGNAME'], \FFI::addr($namebuf));
+            $ret = $this->ffi->ioctl($masterFd, $this->getConstant('TIOCPTYGNAME'), \FFI::addr($namebuf));
             if ($ret === -1) {
                 $errno = $this->getErrno();
                 $error = \FFI::string($this->ffi->strerror($errno));
+                error_log("FFI getSlaveNameFromMaster: Failed to get slave name: {$error} (errno: {$errno})");
                 throw new \RuntimeException("Failed to get slave name: {$error} (errno: {$errno})");
             }
 
-            return \FFI::string($namebuf);
+            $name = \FFI::string($namebuf);
+            error_log("FFI getSlaveNameFromMaster: Got slave name: {$name}");
+
+            return $name;
         } else {
+            error_log('FFI getSlaveNameFromMaster: Using Linux method');
             // Linux: Use TIOCGPTN to get PTY number
             $ptnbuf = $this->ffi->new('unsigned int');
-            $ret = $this->ffi->ioctl($masterFd, $this->constants['TIOCGPTN'], \FFI::addr($ptnbuf));
+            $ret = $this->ffi->ioctl($masterFd, $this->getConstant('TIOCGPTN'), \FFI::addr($ptnbuf));
             if ($ret === -1) {
                 $errno = $this->getErrno();
                 $error = \FFI::string($this->ffi->strerror($errno));
+                error_log("FFI getSlaveNameFromMaster: Failed to get PTY number: {$error} (errno: {$errno})");
                 throw new \RuntimeException("Failed to get PTY number: {$error} (errno: {$errno})");
             }
 
-            return '/dev/pts/'.$ptnbuf->cdata;
+            $name = '/dev/pts/'.$ptnbuf->cdata;
+            error_log("FFI getSlaveNameFromMaster: Got slave name: {$name}");
+
+            return $name;
         }
     }
 
     public function unlockPty(int $masterFd): void
     {
         if (PHP_OS === 'Darwin') {
-            $ret = $this->ffi->ioctl($masterFd, $this->constants['TIOCPTYUNLK'], 0);
+            $ret = $this->ffi->ioctl($masterFd, $this->getConstant('TIOCPTYUNLK'), 0);
             if ($ret === -1) {
                 $errno = $this->getErrno();
                 $error = \FFI::string($this->ffi->strerror($errno));
@@ -182,7 +308,7 @@ class Ffi
             // On Linux, unlock the PTY using TIOCSPTLCK
             $unlockArg = $this->ffi->new('int');
             $unlockArg->cdata = 0;  // 0 = unlock
-            $ret = $this->ffi->ioctl($masterFd, $this->constants['TIOCSPTLCK'], \FFI::addr($unlockArg));
+            $ret = $this->ffi->ioctl($masterFd, $this->getConstant('TIOCSPTLCK'), \FFI::addr($unlockArg));
             if ($ret === -1) {
                 $errno = $this->getErrno();
                 $error = \FFI::string($this->ffi->strerror($errno));
@@ -194,7 +320,7 @@ class Ffi
     public function grantPty(int $masterFd): void
     {
         if (PHP_OS === 'Darwin') {
-            $ret = $this->ffi->ioctl($masterFd, $this->constants['TIOCPTYGRANT'], 0);
+            $ret = $this->ffi->ioctl($masterFd, $this->getConstant('TIOCPTYGRANT'), 0);
             if ($ret === -1) {
                 $errno = $this->getErrno();
                 $error = \FFI::string($this->ffi->strerror($errno));
@@ -212,7 +338,7 @@ class Ffi
         $ws->ws_xpixel = $xPixel;
         $ws->ws_ypixel = $yPixel;
 
-        $ret = $this->ffi->ioctl($masterFd, $this->constants['TIOCSWINSZ'], \FFI::addr($ws));
+        $ret = $this->ffi->ioctl($masterFd, $this->getConstant('TIOCSWINSZ'), \FFI::addr($ws));
         if ($ret === -1) {
             $errno = $this->getErrno();
             $error = \FFI::string($this->ffi->strerror($errno));
@@ -236,7 +362,8 @@ class Ffi
 
     public function setTermios(int $fd, $termios): void
     {
-        $result = $this->ffi->tcsetattr($fd, $this->constants['TCSANOW'], \FFI::addr($termios));
+        error_log("FFI setTermios: Setting terminal attributes for fd {$fd}");
+        $result = $this->ffi->tcsetattr($fd, $this->getConstant('TCSANOW'), \FFI::addr($termios));
 
         if ($result === -1) {
             $errno = $this->getErrno();
